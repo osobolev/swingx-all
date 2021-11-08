38e6fa933fb09adead86fc4a0585b043bc9a715c
Oleg Sobolev <sobolev_ov@mail.ru>
2021-11-08 19:32:04 +0300

    Version 1.7.1: multi-release jars for java 8/9: - move to JDK 17 for build (since JDK-8235229 was fixed only in Java 16) - move to Gradle 7.2 (Gradle 6 does not support Java 17) - use of common build scripts for all my projects (base-lib, module-lib) - changed source roots to used in build scripts (src/src9/resources)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,94 +1,31 @@
 description = "Fork of the inactive swingx-all library"
 
 plugins {
-    `java-library`
-    `maven-publish`
-    `signing`
+    `module-lib`
 }
 
 group = "io.github.osobolev"
-version = "1.7.0"
+version = "1.7.1"
 
-repositories {
-    mavenCentral()
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    withSourcesJar()
-    withJavadocJar()
-}
-
-dependencies {
-    compileOnly("org.kohsuke.metainf-services:metainf-services:1.8")
-    annotationProcessor("org.kohsuke.metainf-services:metainf-services:1.8")
-}
-
-tasks {
-    withType(JavaCompile::class) {
-        options.encoding = "UTF-8"
-        options.compilerArgs.add("-Xlint:deprecation,unchecked")
-    }
-    javadoc {
-        options.encoding = "UTF-8"
-        (options as? StandardJavadocDocletOptions)?.charSet("UTF-8")
-        (options as CoreJavadocOptions).addBooleanOption("Xdoclint:none", true)
-        options.quiet()
-    }
-    jar {
-        manifest.attributes["Implementation-Version"] = project.version
-    }
-}
-
-val sonatypeUsername: String? by project
-val sonatypePassword: String? by project
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            pom {
-                name.set("swingx-all")
-                description.set("Fork of the inactive swingx-all library")
-                url.set("https://github.com/osobolev/swingx-all")
-                licenses {
-                    license {
-                        name.set("GNU General Lesser Public License (LGPL) version 3.0")
-                        url.set("http://www.gnu.org/licenses/lgpl.html")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("Oleg Sobolev")
-                        organizationUrl.set("https://github.com/swingx-all")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/osobolev/swingx-all.git")
-                    developerConnection.set("scm:git:https://github.com/osobolev/swingx-all.git")
-                    url.set("https://github.com/osobolev/swingx-all")
-                }
-            }
-            from(components["java"])
+(publishing.publications["mavenJava"] as MavenPublication).pom {
+    name.set("swingx-all")
+    description.set("Fork of the inactive swingx-all library")
+    url.set("https://github.com/osobolev/swingx-all")
+    licenses {
+        license {
+            name.set("GNU General Lesser Public License (LGPL) version 3.0")
+            url.set("http://www.gnu.org/licenses/lgpl.html")
         }
     }
-
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
-            }
+    developers {
+        developer {
+            name.set("Oleg Sobolev")
+            organizationUrl.set("https://github.com/swingx-all")
         }
     }
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
-}
-
-tasks.named("clean").configure {
-    doLast {
-        project.delete("$projectDir/out")
+    scm {
+        connection.set("scm:git:https://github.com/osobolev/swingx-all.git")
+        developerConnection.set("scm:git:https://github.com/osobolev/swingx-all.git")
+        url.set("https://github.com/osobolev/swingx-all")
     }
 }
---
+++ src9/module-info.java
@@ -0,0 +1,63 @@
+module io.github.osobolev.swingx {
+    exports org.jdesktop.beans;
+    exports org.jdesktop.swingx;
+    exports org.jdesktop.swingx.action;
+    exports org.jdesktop.swingx.auth;
+    exports org.jdesktop.swingx.autocomplete;
+    exports org.jdesktop.swingx.autocomplete.workarounds;
+    exports org.jdesktop.swingx.border;
+    exports org.jdesktop.swingx.calendar;
+    exports org.jdesktop.swingx.color;
+    exports org.jdesktop.swingx.combobox;
+    exports org.jdesktop.swingx.decorator;
+    exports org.jdesktop.swingx.error;
+    exports org.jdesktop.swingx.event;
+    exports org.jdesktop.swingx.geom;
+    exports org.jdesktop.swingx.graphics;
+    exports org.jdesktop.swingx.hyperlink;
+    exports org.jdesktop.swingx.icon;
+    exports org.jdesktop.swingx.image;
+    exports org.jdesktop.swingx.multislider;
+    exports org.jdesktop.swingx.multisplitpane;
+    exports org.jdesktop.swingx.painter;
+    exports org.jdesktop.swingx.painter.effects;
+    exports org.jdesktop.swingx.plaf;
+    exports org.jdesktop.swingx.plaf.basic;
+    exports org.jdesktop.swingx.plaf.basic.core;
+    exports org.jdesktop.swingx.plaf.basic.resources;
+    exports org.jdesktop.swingx.plaf.linux;
+    exports org.jdesktop.swingx.plaf.linux.resources;
+    exports org.jdesktop.swingx.plaf.macosx;
+    exports org.jdesktop.swingx.plaf.macosx.resources;
+    exports org.jdesktop.swingx.plaf.metal;
+    exports org.jdesktop.swingx.plaf.misc;
+    exports org.jdesktop.swingx.plaf.motif;
+    exports org.jdesktop.swingx.plaf.nimbus;
+    exports org.jdesktop.swingx.plaf.synth;
+    exports org.jdesktop.swingx.plaf.windows;
+    exports org.jdesktop.swingx.plaf.windows.resources;
+    exports org.jdesktop.swingx.prompt;
+    exports org.jdesktop.swingx.renderer;
+    exports org.jdesktop.swingx.rollover;
+    exports org.jdesktop.swingx.search;
+    exports org.jdesktop.swingx.sort;
+    exports org.jdesktop.swingx.table;
+    exports org.jdesktop.swingx.text;
+    exports org.jdesktop.swingx.tips;
+    exports org.jdesktop.swingx.tree;
+    exports org.jdesktop.swingx.treetable;
+    exports org.jdesktop.swingx.util;
+
+    requires java.logging;
+    requires java.prefs;
+    requires transitive java.desktop;
+
+    provides org.jdesktop.swingx.plaf.LookAndFeelAddons with
+        org.jdesktop.swingx.plaf.linux.LinuxLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.macosx.MacOSXLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.metal.MetalLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.motif.MotifLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.nimbus.NimbusLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.windows.WindowsClassicLookAndFeelAddons,
+        org.jdesktop.swingx.plaf.windows.WindowsLookAndFeelAddons;
+}
